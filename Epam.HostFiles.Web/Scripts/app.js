04b8bde16b85920543f6b66608a4433c0df30a21
Pavel_Hamanovich <Pavel_Hamanovich@EPBYMINW6134>
2016-04-14 18:10:34 +0300

    Add required javascript frameworks

--- Epam.HostFiles.Web/Scripts/app.js
+++ Epam.HostFiles.Web/Scripts/app.js
@@ -1,1 +1,20 @@
-﻿
+﻿requirejs.config({
+    baseUrl: 'scripts/lib',
+    paths: {
+        models: '../models',
+        collections: '../collections',
+        views: '../views',
+        routers: '../routers',
+        components: '../components',
+        modalDialog: 'backbone.ModalDialog'
+    },
+    shim: {
+        'backbone': {
+            deps: ['underscore', 'jquery'],
+            exports: 'Backbone'
+        },
+        'underscore': {
+            exports: '_'
+        }
+    }
+});
